c24c18e961c8787e02670c3d1d4e5d7bfed69593
HendrikSchmidt <hendriknas@gmail.com>
2020-01-15 22:18:41 +0100

    implement flatmap and zipping for first task in pipeline

--- spark-tutorial/src/main/scala/de/hpi/spark_tutorial/Sindy.scala
+++ spark-tutorial/src/main/scala/de/hpi/spark_tutorial/Sindy.scala
@@ -6,6 +6,23 @@ object Sindy {
 
   def discoverINDs(inputs: List[String], spark: SparkSession): Unit = {
 
-    // TODO
+    import spark.implicits._
+
+    for(file <- inputs){
+      val data = spark.read
+        .option("inferSchema", "true")
+        .option("header", "true")
+        .csv(file)
+        .as[String]
+
+      data.printSchema()
+      data.show(10)
+
+      val columns = data.columns
+
+      val sepColumns = columns.flatMap(row => row.split(";"))
+      val y = data.flatMap(row => row.split(";").zip(sepColumns))
+      y.show()
+    }
   }
 }
