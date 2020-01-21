8326db29dbb193ab6684f40eb11c4a701f89ec93
HendrikSchmidt <hendriknas@gmail.com>
2020-01-21 14:51:26 +0100

    works up to inclusion sets

--- spark-tutorial/src/main/scala/de/hpi/spark_tutorial/Sindy.scala
+++ spark-tutorial/src/main/scala/de/hpi/spark_tutorial/Sindy.scala
@@ -1,6 +1,10 @@
 package de.hpi.spark_tutorial
 
-import org.apache.spark.sql.{Dataset, Row, SparkSession}
+import org.apache.spark.sql.{DataFrame, Dataset, Row, SparkSession}
+import org.apache.spark.sql.types.{IntegerType, StringType, StructField, StructType}
+import org.apache.spark.sql.functions.{collect_set, explode}
+
+import scala.collection.GenIterable
 
 object Sindy {
 
@@ -8,21 +12,28 @@ object Sindy {
 
     import spark.implicits._
 
-    for(file <- inputs){
+    def mapFileToValuesAndColumnNames(file: String): DataFrame = {
       val data = spark.read
         .option("inferSchema", "true")
         .option("header", "true")
         .csv(file)
         .as[String]
 
-      data.printSchema()
-      data.show(10)
-
-      val columns = data.columns
-
-      val sepColumns = columns.flatMap(row => row.split(";"))
-      val y = data.flatMap(row => row.split(";").zip(sepColumns))
-      y.show()
+      val sepColumns = data.columns.flatMap(row => row.split(";"))
+      data.flatMap(row => row.split(";").zip(sepColumns)).toDF("value", "columnName")
     }
+
+    val completeData = inputs.map(file => mapFileToValuesAndColumnNames(file))reduce(_.union(_))
+
+    val aggregatedValues = completeData.groupBy("value").agg(collect_set("columnName"))
+
+    val columnSets = aggregatedValues.select("collect_set(columnName)")
+    columnSets.show()
+
+    val inclusionSetsWithSelf = columnSets.withColumn("columnName", explode($"collect_set(columnName)"))
+    inclusionSetsWithSelf.show()
+
+//    val inclusionSets = inclusionSetsWithSelf.withColumn("includedIn", explode($"collect_set(columnName)"))
+//    inclusionSets.show()
   }
 }
